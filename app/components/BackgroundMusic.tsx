2633f8a89c13f8782e56b368650cb05117264db4
VuongNguyen <vuongnguyenvan282@gmail.com>
2025-11-10 21:41:52 +0700

    update

--- app/components/BackgroundMusic.tsx
+++ app/components/BackgroundMusic.tsx
@@ -36,7 +36,7 @@ export default function BackgroundMusic() {
   return (
     <>
       <audio ref={audioRef} loop>
-        <source src="/music/le-duong.mp3" type="audio/mpeg" />
+        <source src="/music/IThinkTheyCallThisLove.mp3" type="audio/mpeg" />
       </audio>
 
       <button
